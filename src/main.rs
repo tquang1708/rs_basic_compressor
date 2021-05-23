1bbbb91e2e3786096f96b88f1d864567c813712d
tquang1708 <tquang1708@gmail.com>
2021-05-22 23:24:19 -0400

    setting up for implementation of decompress

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
 #[macro_use]
 extern crate clap;
 use clap::App;
+use std::fs;
 
-fn main() {
-    // example of argument parsing from the clap documentation
-    // https://docs.rs/clap/2.33.3/clap/
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let yaml = load_yaml!("cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
 
-    // input is a required arg so we can unwrap safely
     let input_str = matches.value_of("INPUT").unwrap();
+    let input = fs::read(input_str)?;
+
+    // TODO: implement compress/decompress as separate modules in different files
+    if matches.is_present("decompress") {
+        println!("Decompressing file {}...", input_str);
+        println!("Done");
+    } else {
+        println!("Compressing file {}...", input_str);
+        println!("Done");
+    }
+
+    Ok(())
 }
