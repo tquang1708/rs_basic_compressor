689c7b046cda6956297547edb7fb4b341220a1e6
tquang1708 <tquang1708@gmail.com>
2021-05-22 22:20:13 -0400

    set up clap for command line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+#[macro_use]
+extern crate clap;
+use clap::App;
+
 fn main() {
-    println!("Hello, world!");
+    // example of argument parsing from the clap documentation
+    // https://docs.rs/clap/2.33.3/clap/
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
+
+    // input is a required arg so we can unwrap safely
+    let input_str = matches.value_of("INPUT").unwrap();
 }
